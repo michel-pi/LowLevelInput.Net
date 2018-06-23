ebf3df3502c5a0b81d4761e5809be4fa36fbcbe2
Michel <michel.pioch@gmx.de>
2018-06-23 15:52:44 +0200

    implemented pinvoke

---
+++ LowLevelInput/LowLevelInput/PInvoke/Libraries/Kernel32.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Security;
+
+namespace LowLevelInput.PInvoke.Libraries
+{
+    [SuppressUnmanagedCodeSecurity()]
+    internal static class Kernel32
+    {
+        public static GetCurrentThreadId_t GetCurrentThreadId = WinApi.GetMethod<GetCurrentThreadId_t>("kernel32.dll", "GetCurrentThreadId");
+
+        public delegate uint GetCurrentThreadId_t();
+    }
+}
---
+++ LowLevelInput/LowLevelInput/PInvoke/Libraries/User32.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Security;
+
+using LowLevelInput.PInvoke.Types;
+
+namespace LowLevelInput.PInvoke.Libraries
+{
+    [SuppressUnmanagedCodeSecurity()]
+    internal static class User32
+    {
+        public static CallNextHookEx_t CallNextHookEx = WinApi.GetMethod<CallNextHookEx_t>("user32.dll", "CallNextHookEx");
+
+        public static GetMessage_t GetMessage = WinApi.GetMethod<GetMessage_t>("user32.dll", "GetMessageW");
+
+        public static PostThreadMessage_t PostThreadMessage = WinApi.GetMethod<PostThreadMessage_t>("user32.dll", "PostThreadMessageW");
+
+        public static SetWindowsHookEx_t SetWindowsHookEx = WinApi.GetMethod<SetWindowsHookEx_t>("user32.dll", "SetWindowsHookExW");
+
+        public static UnhookWindowsHookEx_t UnhookWindowsHookEx = WinApi.GetMethod<UnhookWindowsHookEx_t>("user32.dll", "UnhookWindowsHookEx");
+
+        public delegate IntPtr CallNextHookEx_t(IntPtr hHook, int nCode, IntPtr wParam, IntPtr lParam);
+
+        public delegate int GetMessage_t(ref Message lpMessage, IntPtr hwnd, uint msgFilterMin, uint msgFilterMax);
+
+        public delegate IntPtr HookProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        public delegate int PostThreadMessage_t(uint threadId, uint msg, IntPtr wParam, IntPtr lParam);
+
+        public delegate IntPtr SetWindowsHookEx_t(int type, IntPtr hookProcedure, IntPtr hModule, uint threadId);
+
+        public delegate int UnhookWindowsHookEx_t(IntPtr hHook);
+    }
+}
