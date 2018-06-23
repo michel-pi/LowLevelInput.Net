aea3898adbc70da2894b5443f5ca2c2dabee8e23
Michel <michel.pioch@gmx.de>
2018-06-23 17:36:40 +0200

    better documentation for KeyState

--- LowLevelInput/LowLevelInput/Hooks/KeyState.cs
+++ LowLevelInput/LowLevelInput/Hooks/KeyState.cs
@@ -3,11 +3,12 @@
 namespace LowLevelInput.Hooks
 {
     /// <summary>
+    /// Defines the state of a key
     /// </summary>
     public enum KeyState : int
     {
         /// <summary>
-        /// The none
+        /// None
         /// </summary>
         None,
 
