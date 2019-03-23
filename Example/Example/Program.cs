5bff09a36a90b4f5bde1c7555fb03e1f60fd8c78
Michel <github@chromadigital.group>
2019-03-23 01:36:39 +0100

    Revert "deleted old project"
    This reverts commit 64f5db6328186a9dfa778e19300d86f9f69a4a9f.

---
+++ Example/Example/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+using LowLevelInput.Converters;
+using LowLevelInput.Hooks;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestFilter();
+
+            return;
+
+            // creates a new instance to capture inputs
+            // also provides IsPressed, WasPressed and GetState methods
+            var inputManager = new InputManager();
+
+            // you may not need those when you use InputManager
+            var keyboardHook = new LowLevelKeyboardHook();
+            var mouseHook = new LowLevelMouseHook();
+
+            // subscribe to the events offered by InputManager
+            inputManager.OnKeyboardEvent += InputManager_OnKeyboardEvent;
+            inputManager.OnMouseEvent += InputManager_OnMouseEvent;
+
+            // same events as above (in case you only need a specific hook and less functionality)
+            keyboardHook.OnKeyboardEvent += KeyboardHook_OnKeyboardEvent;
+            mouseHook.OnMouseEvent += MouseHook_OnMouseEvent;
+
+            // we need to initialize our classes before they fire events and are completely usable
+            inputManager.Initialize();
+            keyboardHook.InstallHook();
+            mouseHook.InstallHook();
+
+            // registers an event (callback) which gets fired whenever the key changes it's state
+            // be sure to use this method after the InputManager is initialized
+            inputManager.RegisterEvent(VirtualKeyCode.Lbutton, InputManager_KeyStateChanged);
+
+            Console.WriteLine("Waiting for up arrow key to exit!");
+
+            // This method will block the current thread until the up arrow key changes it's state to Down
+            // There is no performance penalty (spinning loop waiting for this)
+            inputManager.WaitForEvent(VirtualKeyCode.Up, KeyState.Down);
+
+            // be sure to dispose instances you dont use anymore
+            // not doing so may block windows input and let inputs appear delayed or lagging
+            // these classes try dispose itself when an unhandled exception occurs or the process exits
+            mouseHook.Dispose();
+            keyboardHook.Dispose();
+            inputManager.Dispose();
+        }
+        
+        private static void TestFilter()
+        {
+            LowLevelInput.WindowsHooks.WindowsHookFilter.Filter += (KeyState state, VirtualKeyCode key) =>
+            {
+                if (key == VirtualKeyCode.A) return true; // filter event
+
+                return false; // event passes
+            };
+
+            InputManager manager = new InputManager();
+
+            manager.OnKeyboardEvent += (VirtualKeyCode key, KeyState state) =>
+            {
+                Console.WriteLine("Key: " + key + ", State: " + state);
+            };
+
+            manager.Initialize();
+
+            while(true)
+            {
+                string line = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(line)) continue;
+
+                if (line.ToLower() == "exit") break;
+            }
+        }
+
+        private static void MouseHook_OnMouseEvent(VirtualKeyCode key, KeyState state, int x, int y)
+        {
+            // same as InputManager_OnMouseEvent
+        }
+
+        private static void KeyboardHook_OnKeyboardEvent(VirtualKeyCode key, KeyState state)
+        {
+            // same as InputManager_OnKeyboardEvent
+        }
+
+        private static void InputManager_OnMouseEvent(VirtualKeyCode key, KeyState state, int x, int y)
+        {
+            // x and y may be 0 if there is no data
+            Console.WriteLine("OnMouseEvent: " + KeyCodeConverter.ToString(key) + " - " + KeyStateConverter.ToString(state) + " - X: " + x + ", Y: " + y);
+        }
+
+        private static void InputManager_OnKeyboardEvent(VirtualKeyCode key, KeyState state)
+        {
+            Console.WriteLine("OnKeyboardEvent: " + KeyCodeConverter.ToString(key) + " - " + KeyStateConverter.ToString(state));
+        }
+
+        private static void InputManager_KeyStateChanged(VirtualKeyCode key, KeyState state)
+        {
+            // you may use the same callback for every key or define a new one for each
+            Console.WriteLine("The key state of " + KeyCodeConverter.ToString(key) + " changed to " + KeyStateConverter.ToString(state));
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,42 @@
+# LowLevelInput.Net
+
+A thread safe and event driven Low Level Mouse and Keyboard Hook
+
+### NuGet
+
+	Install-Package LowLevelInput.Net
+
+# Features
+
+- LowLevelMouseHook
+- LowLevelKeyboardHook
+- Events & Callbacks
+- Thread Safe
+- Wait for input without polling
+- Can remove the Injected flag from any input
+- Complete VirtualKeyCodes enum
+
+# Contribute
+
+Clone or download the repository.
+
+Help adding features, fixing issues or provide a better documentation.
+
+# Donate
+Do you like this project?
+
+[![](https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=YJDWMDUSM8KKQ)
+
+```
+BTC 14ES7f4GB3vD1C8Faz6ywqTcdDevxZoMyY
+
+ETH 0xd9E2CB12d310E7BF5E72F591D7A2b8820adced04
+```
+
+# License
+
+-  [LowLevelInput.Net License](https://github.com/michel-pi/LowLevelInput.Net/blob/master/LICENSE  "LowLevelInput.Net License")
+
+# Special Thanks
+
+-  [LeagueRaINi for the WasPressed implementation](https://github.com/LeagueRaINi  "LeagueRaINi for the WasPressed implementation")
