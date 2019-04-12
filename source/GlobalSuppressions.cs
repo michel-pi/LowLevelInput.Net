7eac33d8672cffb077f4a3cb51154e8262acd841
Michel <github@chromadigital.group>
2019-04-13 01:13:52 +0200

    disabled messages

---
+++ source/GlobalSuppressions.cs
@@ -0,0 +1,11 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:LowLevelInput.Hooks.KeyboardHook._hook_HookCalled(System.Object,LowLevelInput.Hooks.HookCalledEventArgs)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:LowLevelInput.Hooks.KeyboardHook._hook_filterCallback(System.Object,LowLevelInput.Hooks.HookCalledEventArgs)~System.Boolean")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:LowLevelInput.Hooks.MouseHook._hook_HookCalled(System.Object,LowLevelInput.Hooks.HookCalledEventArgs)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:LowLevelInput.Hooks.MouseHook._hook_filterCallback(System.Object,LowLevelInput.Hooks.HookCalledEventArgs)~System.Boolean")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>", Scope = "member", Target = "~M:LowLevelInput.Program.Main(System.String[])")]
